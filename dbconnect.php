6e7e4631266f65f41810ca8883eae9da02387516
Tesalonika S.N Kondoy <58241905+tesalonikakondoy27@users.noreply.github.com>
2021-06-16 12:24:41 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","tokobunga");
+$conn = mysqli_connect("sql208.epizy.com","epiz_28817132","yf0TVOr2zfr52bA","epiz_28817132_tokopekita");
 
 if(!$conn){
 	echo "gagal konek database menn";
